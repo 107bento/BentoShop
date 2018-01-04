8d8780e9ede4bcc24fc12667a3a7e2eca5dfa9cb
SafeFly <ast850328@gmail.com>
2018-01-04 11:06:31 +0800

    登入頁面，但還要修改 css

--- src/LoginContainer.js
+++ src/LoginContainer.js
@@ -4,30 +4,30 @@ class LoginContainer extends Component {
   render() {
     return (
       <div className="content">
-        <form class="mx-auto" style={{maxWidth: 46 + 'em'}}>
-        <div class="row">
+        <form className="mx-auto" style={{maxWidth: 46 + 'em'}}>
+        <div className="row">
           <h4>登入系統</h4>
-          <div class="alert alert-danger show fade col-12" id="login-alert" role="alert" hidden>
-            <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> 帳號或密碼錯誤
+          <div className="alert alert-danger show fade col-12" id="login-alert" role="alert" hidden>
+            <i className="fa fa-exclamation-triangle" aria-hidden="true"></i> 帳號或密碼錯誤
           </div>
         </div>
 
-        <div class="form-group row">
-          <div class="input-group">
-            <div class="input-group-addon" id="basic-addon1"><i class="fa fa-user" aria-hidden="true"></i></div>
-            <input class="form-control" id="username" name="username" type="text" autocorrect="off" spellcheck="false" autocapitalize="off" placeholder="帳號" required/>
+        <div className="form-group row">
+          <div className="input-group">
+            <div className="input-group-addon" id="basic-addon1"><i className="fa fa-user" aria-hidden="true"></i></div>
+            <input className="form-control" id="username" name="username" type="text" autocorrect="off" spellcheck="false" autocapitalize="off" placeholder="帳號" required/>
           </div>
         </div>
 
-        <div class="form-group row">
-          <div class="input-group">
-            <div class="input-group-addon" id="basic-addon1"><i class="fa fa-key" aria-hidden="true"></i></div>
-            <input class="form-control" id="password" name="password" type="password" autocorrect="off" spellcheck="false" autocapitalize="off" placeholder="密碼" required/>
+        <div className="form-group row">
+          <div className="input-group">
+            <div className="input-group-addon" id="basic-addon1"><i className="fa fa-key" aria-hidden="true"></i></div>
+            <input className="form-control" id="password" name="password" type="password" autocorrect="off" spellcheck="false" autocapitalize="off" placeholder="密碼" required/>
           </div>
         </div>
 
-        <div class="form-group row">
-          <button class="btn btn-success form-control" id="login-btn" type="submit" onmousedown="event.preventDefault()">登入</button>
+        <div className="form-group row">
+          <button className="btn btn-success form-control" id="login-btn" type="submit" onmousedown="event.preventDefault()">登入</button>
         </div>
         </form>
       </div>
