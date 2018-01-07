7ce2c56af249f9fced9f90eda10ba7ad7eaf858f
SafeFly <ast850328@gmail.com>
2018-01-07 19:34:18 +0800

    登入頁面做好了，也接好 API 了

--- src/LoginContainer.js
+++ src/LoginContainer.js
@@ -1,34 +1,90 @@
 import React, { Component } from 'react';
+import { API } from './API';
 
 class LoginContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formValues: {
+        username: '',
+        password: ''
+      }
+    };
+  }
+  handleSubmit= (e) => {
+    e.preventDefault();
+    // console.log(this);
+    const username = this.state.formValues.username;
+    const password = this.state.formValues.password;
+    
+    API.login(username, password).then(response => {
+      if (response.statusCode === 200) {
+        console.log(response);
+
+        window.location.replace("/");
+      } else if (response.statusCode === 400) {
+        alert('帳號或密碼錯誤，請重新登入');
+        this.setState({
+          formValues: {
+            username: '',
+            password: ''
+          }
+        });
+        console.log(this.state);
+      } else {
+        alert(response.errorMessages);
+        console.log(response.errorMessages);
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
+
+  }
+
+  handleChange = (e) =>  {
+    e.preventDefault();
+    let formValues = this.state.formValues;
+    let name = e.target.name;
+    let value = e.target.value;
+
+    formValues[name] = value;
+
+    this.setState({formValues})
+  }
+
+  mouseDown = (e) => {
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <div className="content">
-        <form className="mx-auto" style={{maxWidth: 46 + 'em'}}>
-        <div className="row">
-          <h4>登入系統</h4>
-          <div className="alert alert-danger show fade col-12" id="login-alert" role="alert" hidden>
-            <i className="fa fa-exclamation-triangle" aria-hidden="true"></i> 帳號或密碼錯誤
+      <div className="content mx-auto my-5" style={{width: 235}}>
+        <form onSubmit={this.handleSubmit}>
+          <div className="row">
+            <h4>登入</h4>
+            <div className="alert alert-danger show fade col-12" id="login-alert" role="alert" hidden>
+              <i className="fa fa-exclamation-triangle" aria-hidden="true"></i> 帳號或密碼錯誤
+            </div>
           </div>
-        </div>
 
-        <div className="form-group row">
-          <div className="input-group">
-            <div className="input-group-addon" id="basic-addon1"><i className="fa fa-user" aria-hidden="true"></i></div>
-            <input className="form-control" id="username" name="username" type="text" autocorrect="off" spellcheck="false" autocapitalize="off" placeholder="帳號" required/>
+          <div className="form-group row">
+            <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-user" aria-hidden="true"></i></div>
+              <input className="form-control" name="username" value={this.state.formValues.username} type="text" placeholder="帳號" required onChange={this.handleChange}/>
+            </div>
           </div>
-        </div>
 
-        <div className="form-group row">
-          <div className="input-group">
-            <div className="input-group-addon" id="basic-addon1"><i className="fa fa-key" aria-hidden="true"></i></div>
-            <input className="form-control" id="password" name="password" type="password" autocorrect="off" spellcheck="false" autocapitalize="off" placeholder="密碼" required/>
+          <div className="form-group row">
+            <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-key" aria-hidden="true"></i></div>
+              <input className="form-control" name="password" value={this.state.formValues.password} type="password" placeholder="密碼" required onChange={this.handleChange}/>
+            </div>
           </div>
-        </div>
 
-        <div className="form-group row">
-          <button className="btn btn-success form-control" id="login-btn" type="submit" onmousedown="event.preventDefault()">登入</button>
-        </div>
+          <div className="form-group row">
+            <button className="btn btn-success form-control" id="login-btn" type="submit" onMouseDown={this.mouseDown}>登入</button>
+          </div>
         </form>
       </div>
     );
