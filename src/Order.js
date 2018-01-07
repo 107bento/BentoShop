38318dc520711cdb09e8e94e650a68707c61464f
SafeFly <ast850328@gmail.com>
2018-01-07 19:33:55 +0800

    巢狀的 Orders

---
+++ src/Order.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+class Order extends React.Component {
+
+  render() {
+    return (
+    <div className="mx-auto" style={{maxWidth: '75%'}}> 
+        <div className="my-3 text-center" id="today">
+            {this.props.match.params.orderDate}
+        </div>
+        <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">餐點名稱</th>
+                <th scope="col" className="text-right">份數</th>
+                <th scope="col" className="text-right">金額</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>山雞肉飯</td>
+                <td className="text-right">10</td>
+                <td className="text-right">700</td>
+              </tr>
+              <tr>
+                <td>滷肉飯</td>
+                <td className="text-right">5</td>
+                <td className="text-right">350</td>
+              </tr>
+              <tr>
+                    <td>餐點三</td>
+                    <td className="text-right">0</td>
+                    <td className="text-right">0</td>
+                </tr>
+              <tr className="font-weight-bold">
+                <td scope="row" colSpan="2" >總金額</td>
+                <td className="text-right">1050</td>
+              </tr>
+            </tbody>
+          </table>
+    </div>
+    );
+  }
+}
+ 
+export default Order
