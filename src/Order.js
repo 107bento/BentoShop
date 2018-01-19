57338c60b2d1f4cb72e39a1c039cb2c439479f3f
SafeFly <ast850328@gmail.com>
2018-01-19 20:32:03 +0800

    按 返回列表 可以返回 orders，剩價錢還沒有寫好

--- src/Order.js
+++ src/Order.js
@@ -3,20 +3,30 @@ import {Button} from 'reactstrap';
 
 class Order extends React.Component {
 
-  render() {
+  constructor(props) {
+    super(props);
+    this.returnOrderList = this.returnOrderList.bind(this);
+  }
+
+  returnOrderList() {
+    window.location.replace('/orders');
+  }
+
+  render() {
+    console.log(window.order);
+    const order = window.order ? window.order : {meals: []};
+    if (order.meals.length == 0) {
+      alert("Order not found!");
+      window.location.replace('/orders');
+    }
+    let total = 0;
 
     return (
     <div className="mx-auto" style={{maxWidth: '45rem'}}>
-      <h4 className="my-2"><span className="badge badge-primary">返回列表</span></h4>
+      <h4 className="my-2" onClick={this.returnOrderList}><span className="badge badge-primary">返回列表</span></h4>
       <div className="row my-3">
-        <div className="col-1 text-center">
-            <i className="fa fa-caret-left" aria-hidden="true" style={{fontSize: '2em'}}></i>
-        </div>
-        <div className="text-center col-10" id="today">
+        <div className="text-center col-12" id="today">
           {this.props.match.params.orderDate}
-        </div>
-        <div className="col-1 text-center">
-            <i className="fa fa-caret-right" aria-hidden="true" style={{fontSize: '2em'}}></i>
         </div>
       </div>
       <table className="table">
@@ -28,21 +38,18 @@ class Order extends React.Component {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>山雞肉飯</td>
-            <td className="text-right">10</td>
-            <td className="text-right">700</td>
-          </tr>
-          <tr>
-            <td>滷肉飯</td>
-            <td className="text-right">5</td>
-            <td className="text-right">350</td>
-          </tr>
-          <tr>
-                <td>餐點三</td>
-                <td className="text-right">0</td>
-                <td className="text-right">0</td>
-            </tr>
+          {
+            order.meals.map((meal, i) => {
+              // total += meal.meal_price * meal.amount;
+              return (
+                <tr key={i}>
+                  <td>{meal.meal_name}</td>
+                  <td className="text-right">{meal.amount}</td>
+                  <td className="text-right">700</td>
+                </tr>
+              );
+            })
+          }
           <tr className="font-weight-bold">
             <td scope="row" colSpan="2" >總金額</td>
             <td className="text-right">1050</td>
